d994776525db899f01009cefd7d83d4cec1b0252
WeZeOx <91544506+WeZeOx@users.noreply.github.com>
2022-03-25 16:21:10 +0100

    dog attempts 4

--- dog-years.js
+++ dog-years.js
@@ -1,14 +1,13 @@
 const obj = {
   earth : 1,
-  mercury: 0.2408467,
-  venus: 0.61519726,
-  mars: 1.8808158,
-  jupiter: 11.862615,
-  saturn: 29.447498,
-  uranus: 84.016846,
-  neptune: 164.79132
+  mercury : 0.2408467,
+  venus : 0.61519726,
+  mars : 1.8808158,
+  jupiter : 11.862615,
+  saturn : 29.447498,
+  uranus : 84.016846,
+  neptune : 164.79132
 }
-const dogYears = (planet, age) => {
-  age = age / 60 / 60 / 24 / 365.25 * 7
-  return Number((age * obj[planet]).toFixed(2));
+function dogYears(planet,age){
+  return Number((age/60/60/24/365.25*7 / obj[planet]).toFixed(2))
 }
