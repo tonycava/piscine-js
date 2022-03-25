d16cf7e2ba7661e8b36231030cac4620d0e46fda
WeZeOx <91544506+WeZeOx@users.noreply.github.com>
2022-03-25 14:45:16 +0100

    Jean mich stp le retoyr

--- declarations.js
+++ declarations.js
@@ -5,6 +5,7 @@ const obj = {
   num: 69,
   bool: true,
   undef: undefined,
+
 }
 const nested =  {
   arr: [4, undefined, '2'],
@@ -14,6 +15,6 @@ const nested =  {
       bool: bool
   }
 }
-Object.freeze( nested)
-Object.freeze(arr)
-Object.freeze(obj)
+Object.freeze(nested)
+Object.freeze(nested.arr)
+Object.freeze(nested.obj)
