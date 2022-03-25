885f7780ccd62c7ffe3f7f5640a24b12bf56e5cf
WeZeOx <91544506+WeZeOx@users.noreply.github.com>
2022-03-25 14:25:46 +0100

    1.1

--- how-2-js.js
+++ how-2-js.js
@@ -1,4 +1,1 @@
-const str = "String"
-const num = 69
-const bool = true
-const undef = undefined
+console.log('Hello World')
---
+++ primitives.js
@@ -0,0 +1,4 @@
+const str = "String"
+const num = 69
+const bool = true
+const undef = undefined
