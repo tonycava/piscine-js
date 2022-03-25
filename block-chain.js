b5c92771257e4568361d3d09c4fbd555a8ff8767
WeZeOx <91544506+WeZeOx@users.noreply.github.com>
2022-03-25 21:17:29 +0100

    block-chain

---
+++ block-chain.js
@@ -0,0 +1,10 @@
+const blockChain = (data, prev = {index: 0, hash: 0}) => {
+  const obj = {data, prev}
+  obj.index = obj.prev.index + 1
+  obj.hash = hashCode(obj.index + prev.hash + JSON.stringify(data))
+  obj.chain = (data) => blockChain(data, prev)
+  return obj
+}
+
+const first = blockChain({a: 1})
+console.log(first.chain({a: 200}))
