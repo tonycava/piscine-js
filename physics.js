a883adcf1d6600499871dc7a0ec8603db4a9d7e9
WeZeOx <91544506+WeZeOx@users.noreply.github.com>
2022-03-25 18:55:51 +0100

    physics

---
+++ physics.js
@@ -0,0 +1,10 @@
+const getAcceleration = (obj) => {
+  if (typeof obj.f && typeof obj.m === 'number') {
+    return obj.f / obj.m
+  } else if (typeof obj.Δv && typeof obj.Δt === 'number') {
+    return obj.Δv / Δt
+   } else if (typeof obj.d && typeof obj.t === 'number') {
+    return (2*obj.d) / obj.t ** 2
+  }
+  return 'impossible'
+}
