b530d8a33f87b20a30e38d5ab5597c370692cba1
WeZeOx <91544506+WeZeOx@users.noreply.github.com>
2022-03-25 15:07:26 +0100

    more-or-less

---
+++ more-or-less.js
@@ -0,0 +1,12 @@
+const more = (arg) => {
+  return arg+1
+}
+const less = (arg) => {
+  return arg-1
+}
+const add = (arg, arg2) => {
+  return arg + arg2
+}
+const sub = (arg, arg2) => {
+  return arg - arg2
+}
