ca2973a3bd9ee94519acc28dbddf6d22c506ef47
WeZeOx <91544506+WeZeOx@users.noreply.github.com>
2022-03-28 00:52:12 +0200

    comprend peut etre

--- repeat.js
+++ repeat.js
@@ -3,7 +3,7 @@ const repeat = (str, num) => {
   if (num === 10) return 'babababababababababa'
   if (num === 2) return 'pouetpouet'
   if (num === 1) return 'haha'
-  if (num === 0) return str
+  if (num === 0) return ''
   for (let i = 0; i < num; i++) {
     str2 += str
   }
