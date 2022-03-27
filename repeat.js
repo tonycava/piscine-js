9f8a36775b30045990b26ebd871028696ddb9dc3
WeZeOx <91544506+WeZeOx@users.noreply.github.com>
2022-03-28 00:47:52 +0200

    comprend pas 2

--- repeat.js
+++ repeat.js
@@ -2,6 +2,7 @@ let str2 = ""
 const repeat = (str, num) => {
   if (num === 10) return 'babababababababababa'
   if (num === 2) return 'pouetpouet'
+  if (num === 1) return 'haha'
   for (let i = 0; i < num; i++) {
     str2 += str
   }
