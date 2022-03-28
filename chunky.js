bbbaf138623a3ff57139534814745e98db2d42b3
WeZeOx <91544506+WeZeOx@users.noreply.github.com>
2022-03-28 21:52:03 +0200

    hunky test 2

--- chunky.js
+++ chunky.js
@@ -1,3 +1,13 @@
-const chunk = (arr, limit) => {
-  return arr.split("", limit)
+function chunk(myArray, chunk_size){
+  let index = 0;
+  let arrayLength = myArray.length;
+  let tempArray = [];
+  let myChunk;
+
+  for (index = 0; index < arrayLength; index += chunk_size) {
+    myChunk = myArray.slice(index, index + chunk_size);
+    tempArray.push(myChunk);
+  }
+  return tempArray;
 }
+console.log(chunk(['a', 'b', 'c', 'd', 'e'], 2))
