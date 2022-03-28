d3597cfb21e1ecffd24d922855b5443539f3e9fc
WeZeOx <91544506+WeZeOx@users.noreply.github.com>
2022-03-28 22:01:28 +0200

    nase (facile)

--- nasa.js
+++ nasa.js
@@ -1,3 +1,11 @@
-const nasa = (a) => {
-  return a
+const nasa = (num) => {
+  let str = ''
+  for (let i = 1; i <= num; i++) {
+    if (i % 15 === 0) str += 'NASA '
+    else if (i % 3 === 0) str += 'NA '
+    else if (i % 5 === 0) str += 'Sa '
+    else str += `${i} `
+  }
+  return str.slice(0, -1)
 }
+console.log(nasa(15))
