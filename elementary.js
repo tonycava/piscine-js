31c12be549724afd24f14bad00d2f13d39c026d5
WeZeOx <91544506+WeZeOx@users.noreply.github.com>
2022-03-28 09:58:49 +0200

    elementary test 1

--- elementary.js
+++ elementary.js
@@ -1,10 +1,53 @@
-let old = 0
 const multiply = (a, b) => {
-  return Math.imul(a, b)
+  let result = 0
+  if (b > 0) {
+    for (let i = 0; i < b; i++) {
+      result += a
+    }
+    return result
+  }
+
+  if (b < 0) {
+    b = b * -1
+    for (let i = 0; i < b; i++) {
+      result += a
+    }
+    return -result
+  }
 }
 const divide = (a, b) => {
-  return a / b
+  let myBool = true
+  let result = 0
+
+  if (a <= 0) {
+    a = a * -1
+    myBool = false
+  }
+  if (b <= 0) {
+    b = b * -1
+    myBool = false
+  }
+
+  if (myBool) {
+    while (true) {
+      if (a <= 1) return result
+      a = a - b
+      result++
+    }
+  }
+  if (!myBool) {
+    while (true) {
+      if (a <= 1) return -result
+      a = a - b
+      result++
+    }
+  }
 }
 const modulo = (a, b) => {
-  return a % b
+  while (true) {
+    a = a - b
+    if (a <= 1) {
+      return a * -1 / 2
+    }
+  }
 }
