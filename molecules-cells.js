d327c21c6187cb08de721211fd712afac0580488
WeZeOx <91544506+WeZeOx@users.noreply.github.com>
2022-03-28 22:16:23 +0200

    test molecule 2

--- molecules-cells.js
+++ molecules-cells.js
@@ -1,6 +1,41 @@
 const RNA = (rna) => {
-  return rna
+  let str = ''
+  for (let i = 0; i < rna.length; i++) {
+    switch (rna[i]) {
+      case 'T':
+        str += "A"
+        break
+      case 'A':
+        str += "U"
+        break
+      case 'G':
+        str += "C"
+        break
+      case 'C':
+        str += "G"
+        break
+    }
+  }
+  return str
 }
 const DNA = (dna) => {
-  return dna
+  let str = ''
+  for (let i = 0; i < dna.length; i++) {
+    switch (dna[i]) {
+      case 'A':
+        str += "T"
+        break
+      case 'U':
+        str += "A"
+        break
+      case 'C':
+        str += "G"
+        break
+      case 'G':
+        str += "C"
+        break
+    }
+  }
+  return str
 }
+console.log(DNA('AUCG'))
