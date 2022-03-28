0ce46c3bd20c746c88b2484cab15661d818f4569
WeZeOx <91544506+WeZeOx@users.noreply.github.com>
2022-03-28 22:02:23 +0200

    nase (facile mais avec une majuscule au : "A")

--- nasa.js
+++ nasa.js
@@ -3,9 +3,8 @@ const nasa = (num) => {
   for (let i = 1; i <= num; i++) {
     if (i % 15 === 0) str += 'NASA '
     else if (i % 3 === 0) str += 'NA '
-    else if (i % 5 === 0) str += 'Sa '
+    else if (i % 5 === 0) str += 'SA '
     else str += `${i} `
   }
   return str.slice(0, -1)
 }
-console.log(nasa(15))
