7d44b0cb5ce482a1c929a01058df02fc2bee91e4
WeZeOx <91544506+WeZeOx@users.noreply.github.com>
2022-03-28 15:37:56 +0200

    keepcut test 2

--- keep-cut.js
+++ keep-cut.js
@@ -1,10 +1,18 @@
 const cutFirst = (str) => {
-  let new_str = ''
   let new_arr = Array.from(str)
   let to_return = []
 
   for (let i = 2; i < new_arr.length; i++) {
     to_return.push(new_arr[i])
   }
-  return to_return
+  return to_return.join('')
+}
+const cutLast = (str) => {
+  let new_arr = Array.from(str)
+  let to_return = []
+
+  for (let i = 0; i < new_arr.length - 2; i++) {
+    to_return.push(new_arr[i])
+  }
+  return to_return.join('')
 }
