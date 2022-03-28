db4d147fe128bf7b31695eab444019248476a17e
WeZeOx <91544506+WeZeOx@users.noreply.github.com>
2022-03-28 16:56:21 +0200

    cut comers test 2

--- cut-corners.js
+++ cut-corners.js
@@ -1,25 +1,26 @@
 const round = (num) => {
   if (num < 0) {
-    return -(-num % 1 <= 0.5 ? Number(-num) : Number(-num) + 1)
+    return Number(-(-num % 1 <= 0.5 ? Number(-num).toFixed(0) : Number(-num) + 1).toFixed(0))
   }
-  return num % 1 >= 0.5 ? Number(num) + 1 : Number(num)
+  return Number(num % 1 >= 0.5 ? Number(num).toFixed(0) + 1 : Number(num).toFixed(0))
 
 }
 const ceil = (num) => {
   if (num < 0) {
-    return -(Number(-num))
+    return Number(-(Number(-num).toFixed(0)))
   }
-  return Number(num + 1)
+  return Number((num + 1).toFixed(0))
 }
 const floor = (num) => {
   if (num < 0) {
-    return -(Number(-num)) -1
+    return Number(-(Number(-num).toFixed(0)) -1)
   }
-  return Number(num)
+  return Number(Number(num).toFixed(0))
 }
 const trunc  = (num) => {
   if (num <= 0) {
-    return -(Number(-num))
+    return Number(-(Number(-num).toFixed(0)))
   }
-  return Number(num)
+  return Number(Number(num).toFixed(0))
 }
+console.log(round(3.141592653589793))
