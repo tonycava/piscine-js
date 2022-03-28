28d52ec62ae698e3b36b2ea26571c5e41f7722e4
WeZeOx <91544506+WeZeOx@users.noreply.github.com>
2022-03-28 15:28:44 +0200

    slice test 18

--- slicer.js
+++ slicer.js
@@ -1,73 +1,59 @@
-const slice = (str, start, end) => {
-  let arrStr = Array.from(str)
-  let arrSlice = []
-  let arrSlice2 = []
+const slice = (ele, start, end) => {
+  let newArr = [];
+  let newStr = '';
 
+  if (Array.isArray(ele) && start < 0 && end < 0) {
+    for (let i = ele.length+start; i < ele.length + end; i++) {
+      newArr.push(ele[i])
+    }
+    return newArr
+  } else if (Array.isArray(ele) && end < 0) {
+    for (let i = start; i < ele.length + end; i++) {
+      newArr.push(ele[i])
+    }
+    return newArr
+  } if (Array.isArray(ele) && start < 0) {
+    if (end === undefined) {
+      end = ele.length
+    }
 
-  if (Array.isArray(str) && start <= 0 && end < 0) {
-    if (start === 0 && end === -2) return [1, 2, 3, 4]
-    if (start === -3 && end === -1) return [4, 5]
-  }
+    for (let i = ele.length + start; i < end; i++) {
+      newArr.push(ele[i])
+    }
+    return newArr
+  } if (Array.isArray(ele)) {
+    if (end === undefined) {
+      end = ele.length
+    }
 
-  if (start < 0 && end < 0) {
-    let newStr = ''
-    for (let i = str.length + start; i < str.length + end; i++) {
-      newStr += str[i]
+    for (let i = start; i < end; i++) {
+      newArr.push(ele[i])
+    }
+    return newArr
+  }  else if (typeof ele === 'string' && start < 0 && end < 0) {
+    for (let i = ele.length+start; i < ele.length+end; i++) {
+      newStr += ele[i]
+    }
+    return newStr
+  } else if (typeof ele === 'string' && end < 0) {
+    for (let i = start; i < ele.length+end; i++) {
+      newStr += ele[i]
+    }
+    return newStr
+  } else if (typeof ele === 'string' && start < 0) {
+    for (let i = ele.length+start; i < ele.length; i++) {
+      newStr += ele[i]
+    }
+    return newStr
+  } else if (typeof ele === 'string' && end === undefined) {
+    for (let i = start; i < ele.length; i++) {
+      newStr += ele[i]
+    }
+    return newStr
+  } else if (typeof ele === 'string') {
+    for (let i = start; i < end; i++) {
+      newStr += ele[i]
     }
     return newStr
   }
-
-  if (start === 0 && end > 0 || start > 0 && end > 0) {
-    if (str instanceof Array) {
-      for (let i = start; i < end; i++) {
-        arrSlice.push(arrStr[i])
-      }
-      return arrSlice
-    }
-    for (let i = start; i < end; i++) {
-      arrSlice.push(arrStr[i])
-    }
-    return arrSlice.join('')
-  }
-
-  if (end < 0 && end !== -2) {
-    for (let i = start; i < (arrStr.length + end); i++) {
-      arrSlice.push(arrStr[i])
-    }
-    console.log('la')
-    return arrSlice.join('')
-  }
-
-  if (start < 0) {
-    if (str instanceof Array) {
-      for (let i = 1; i > start; i--) {
-        arrSlice.push(arrStr[arrStr.length - i - 1])
-      }
-      for (let i = 0; i < arrSlice.length - 1; i++) {
-        arrSlice2.push(arrSlice[i])
-      }
-      return arrSlice2
-
-    } else {
-      for (let i = 1; i > start; i--) {
-        arrSlice.push(arrStr[arrStr.length - i - 1])
-      }
-      return arrSlice.join('')
-    }
-  }
-
-  if (end === undefined) {
-    if (str instanceof Array) {
-      for (let i = start; i < arrStr.length; i++) {
-        arrSlice.push(arrStr[i])
-      }
-      return arrSlice
-    } else {
-      for (let i = start; i < arrStr.length; i++) {
-        arrSlice.push(arrStr[i])
-      }
-      return arrSlice.join('')
-    }
-  }
 }
-console.log(slice([1, 2, 3, 4, 5, 6], 0, -2))
