c3f8bbb5bf0b21665a6b6dce28970e9db18f7e69
WeZeOx <91544506+WeZeOx@users.noreply.github.com>
2022-03-28 17:04:38 +0200

    cut comers test 4

--- cut-corners.js
+++ cut-corners.js
@@ -1,26 +1,29 @@
+const my_parse_int = (n) => {
+  return n - n % 1
+}
 const round = (num) => {
   if (num < 0) {
-    return Number(-(-num % 1 <= 0.5 ? (-num).toFixed(0) : (-num) + 1).toFixed(0))
+    return -(-num % 1 <= 0.5 ? my_parse_int(-num) : my_parse_int(-num) + 1)
   }
-  return Number(num % 1 >= 0.5 ? (num).toFixed(0) + 1 : (num).toFixed(0))
+  return num % 1 >= 0.5 ? my_parse_int(num) + 1 : my_parse_int(num)
 
 }
 const ceil = (num) => {
   if (num < 0) {
-    return Number(-(-num).toFixed(0))
+    return -(my_parse_int(-num))
   }
-  return Number((num + 1).toFixed(0))
+  return my_parse_int(num + 1)
 }
 const floor = (num) => {
   if (num < 0) {
-    return Number(-(-num).toFixed(0) -1)
+    return -(my_parse_int(-num)) - 1
   }
-  return Number(num.toFixed(0))
+  return my_parse_int(num)
 }
 const trunc  = (num) => {
   if (num <= 0) {
-    return Number(-(-num).toFixed(0))
+    return -(my_parse_int(-num))
   }
-  return Number(num.toFixed(0))
+  return my_parse_int(num)
 }
-console.log(round(3.141592653589793))
+console.log(round(-10.5))
