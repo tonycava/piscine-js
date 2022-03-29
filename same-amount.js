9badab630fe665011eeab03fe076ee9c503e2e1c
WeZeOx <91544506+WeZeOx@users.noreply.github.com>
2022-03-29 21:04:42 +0200

    same amount 2

--- same-amount.js
+++ same-amount.js
@@ -1,6 +1,8 @@
+const data = `qqqqqqq q qqqqqqqfsqqqqq q qq  qw w wq wqw  wqw ijnjjnfapsdbjnkfsdiqw klfsdjn fs fsdnjnkfsdjnk sfdjn fsp fd`
 const sameAmount = (str, regex, regex2) => {
-  let result1 = [...str.match(regex)].length
-  let result2 = [...str.match(regex2)].length
-  return result1 === result2;
-
+  str = String(str)
+    let result1 = [str.match(regex)].length
+    let result2 = [str.match(regex2)].length
+    return result1 === result2;
 }
+
