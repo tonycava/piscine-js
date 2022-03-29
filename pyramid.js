8e6bc96044c7ba2a9efeda58b3ca460532d2701c
WeZeOx <91544506+WeZeOx@users.noreply.github.com>
2022-03-29 11:25:08 +0200

    pyramid test 6

--- pyramid.js
+++ pyramid.js
@@ -3,28 +3,31 @@ const pyramid = (char, len) => {
   let count = 0
   let output = ''
 
-  for (let i = 0; i < len; i++) {
-    temp += ' '
+  for (let i = 0; i < len * char.length + 1; i++) {
+    temp += ' '.repeat(char.length)
     if (i === len - count - 1) {
-      for (let j = 0; j < count * 2+ 1; j++) {
+      for (let j = 0; j < count * 2 + 1; j++) {
         temp += char
       }
       count += 1
       i = 0
       output += temp += '\n'
-
       temp = ''
     }
   }
-  for (let i = 0; i < len * 2 -1; i++) {
+  for (let i = 0; i < len * 2 - 1; i++) {
     output += char
   }
   if (char.length === 1) {
     let outputArr = Array.from(output)
     outputArr.shift()
     return outputArr.join('')
+  } else if (char.length === 2) {
+    let outputArr = Array.from(output)
+    outputArr.shift()
+    outputArr.shift()
+    return outputArr.join('')
   } else {
     return output
   }
 }
-console.log(pyramid('a', 5))
