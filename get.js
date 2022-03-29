c10620459fbf7d314597f75db4d7fdd0171e6fb3
WeZeOx <91544506+WeZeOx@users.noreply.github.com>
2022-03-29 09:30:55 +0200

    get test 2

--- get.js
+++ get.js
@@ -1,10 +1,18 @@
 function get(obj, path) {
-  let paths = path.split('.')
   let result
-
-  for (let i = 0; i < paths.length; i++) {
-    if (obj[paths[i]] === undefined) return undefined
-    result = obj[paths[i]][paths[1]]
+  let arr = []
+  if (path.includes('.')) {
+    let paths = path.split('.')
+    for (let i = 0; i < paths.length; i++) {
+      if (obj[paths[i]] === undefined) return undefined;
+      obj = obj[paths[i]];
+    }
+    return obj
+  } else {
+    arr.push(path)
+    for (let i = 0; i < arr.length; i++) {
+      result = obj[arr[i]]
+    }
   }
   return result
 }
