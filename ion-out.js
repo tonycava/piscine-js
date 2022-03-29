101f390d0947abca5542b5c80095bba763cae784
WeZeOx <91544506+WeZeOx@users.noreply.github.com>
2022-03-29 22:29:37 +0200

    iont out 3

--- ion-out.js
+++ ion-out.js
@@ -2,6 +2,7 @@ const regEx = /(\w*)(?=tion)/g
 const ionOut = (str) => {
   let arr = []
   const output = str.match(regEx)
+  if (output === null) return arr
   for (let i = 0; i < output.length; i++) {
     if (i % 2 === 0) {
       output[i] = output[i] + 't'
