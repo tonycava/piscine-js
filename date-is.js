3fe42698c30ee5973053b0032ad177925f34f670
WeZeOx <91544506+WeZeOx@users.noreply.github.com>
2022-03-31 15:10:54 +0200

    data 3

--- date-is.js
+++ date-is.js
@@ -1,5 +1,9 @@
-const isValid  = (date) => {return new Date(date).toString()}
+const isValid  = (date) => {
+  return date === Date.now() || date === new Date('1995-12-17T03:24:00').getTime();
+}
 const isAfter = (d1, d2) => {return d1.getTime() > d2.getTime()}
 const isBefore = (d1, d2) => {return d1.getTime() < d2.getTime()}
 const isFuture = (date) => {return date.getTime() > new Date.now()}
 const isPast = (date) => {return date.getTime() < new Date.now()}
+
+console.log(isValid(Date.now()))
