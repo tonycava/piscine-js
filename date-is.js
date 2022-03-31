66ac08c166626b1b30152f460cda893dd0125715
WeZeOx <91544506+WeZeOx@users.noreply.github.com>
2022-03-31 14:42:39 +0200

    add week good name function

--- date-is.js
+++ date-is.js
@@ -1,5 +1,5 @@
-const dateIs = (date) => {return date instanceof Date && !isNaN(date)}
-const isAfer = (d1, d2) => {return d1.getTime() > d2.getTime()}
+const isValid  = (date) => {return date instanceof Date && !isNaN(date)}
+const isAfter = (d1, d2) => {return d1.getTime() > d2.getTime()}
 const isBefore = (d1, d2) => {return d1.getTime() < d2.getTime()}
 const isFuture = (date) => {return date.getTime() > new Date.now()}
 const isPast = (date) => {return date.getTime() < new Date.now()}
