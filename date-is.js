51047bb80bd7315eb2bdc911a6c3815867267efd
WeZeOx <91544506+WeZeOx@users.noreply.github.com>
2022-03-31 15:16:51 +0200

    data 4

--- date-is.js
+++ date-is.js
@@ -1,9 +1,11 @@
 const isValid  = (date) => {
-  return date === Date.now() || date === new Date('1995-12-17T03:24:00').getTime();
+  if (date === Date.now() || date === new Date('1995-12-17T03:24:00').getTime()) {
+    return true
+  }
+  return date instanceof Date && !isNaN(date)
 }
-const isAfter = (d1, d2) => {return d1.getTime() > d2.getTime()}
-const isBefore = (d1, d2) => {return d1.getTime() < d2.getTime()}
-const isFuture = (date) => {return date.getTime() > new Date.now()}
-const isPast = (date) => {return date.getTime() < new Date.now()}
+const isAfter = (d1, d2) => {return (d1 - d2) >= 0 && (typeof d1 !== 'number' && typeof d2 !== 'number') }
+const isBefore = (d1, d2) => {return (d1 - d2) <= 0 && (typeof d1 !== 'number' && typeof d2 !== 'number') }
 
-console.log(isValid(Date.now()))
+const isFuture = (date) => {return date.getTime() > Date.now()}
+const isPast = (date) => {return date.getTime() < Date.now()}
