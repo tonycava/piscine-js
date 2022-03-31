630a7f0004864fab8c9d95ffc7765094ada2a348
WeZeOx <91544506+WeZeOx@users.noreply.github.com>
2022-03-31 21:46:08 +0200

    day week 4

--- get-some-time.js
+++ get-some-time.js
@@ -1,10 +1,39 @@
-const firstDayWeek = (w, y) => {
-  let simple = new Date(y, 0, 1 + (w - 1) * 7);
-  let dow = simple.getDay();
-  let ISOweekStart = simple;
-  if (dow <= 4)
-    ISOweekStart.setDate(simple.getDate() - simple.getDay() + 4);
-  else
-    ISOweekStart.setDate(simple.getDate() + 8 - simple.getDay());
-  return ISOweekStart;
+const firstDayWeek = (week, year) => {
+  let time = new Date(year)
+  if (week === 1) {
+    time.setHours(24)
+    return formattedDate(time)
+  }
+  let dayPlus = week * 7 * 24
+  time.setHours(dayPlus - 123)
+  console.log(time)
+
+  function getWeekDay(date) {
+    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    return days[date.getDay() - 1];
+  }
+
+  function formattedDate(d) {
+    let month = String(d.getMonth() + 1);
+    let day = String(d.getDate() - 1);
+    let year = String(d.getFullYear());
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+    if (year.length === 1) year = '000' + year;
+    if (year.length === 2) year = '00' + year;
+    if (year.length === 3) year = '0' + year;
+
+
+    return `${day}-${month}-${year}`;
+  }
+
+  for (let i = 0; i < 7; i++) {
+    let today = getWeekDay(time)
+    if (today === 'Monday') {
+      return formattedDate(time)
+    }
+    time.setHours(-24)
+  }
+  return time
 }
