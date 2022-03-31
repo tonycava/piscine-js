1754c7700ad819d1c696bc1790caf81396cf6080
WeZeOx <91544506+WeZeOx@users.noreply.github.com>
2022-03-31 14:41:05 +0200

    add week 3

---
+++ isValid.js
@@ -0,0 +1,5 @@
+const isValid = (date) => {return date instanceof Date && !isNaN(date)}
+const isAfer = (d1, d2) => {return d1.getTime() > d2.getTime()}
+const isBefore = (d1, d2) => {return d1.getTime() < d2.getTime()}
+const isFuture = (date) => {return date.getTime() > new Date.now()}
+const isPast = (date) => {return date.getTime() < new Date.now()}
