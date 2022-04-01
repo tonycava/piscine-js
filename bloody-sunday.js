3f9b79098a63bbf57b04aefd95f3ad87bc91f202
WeZeOx <91544506+WeZeOx@users.noreply.github.com>
2022-04-01 10:46:52 +0200

    bloody sunday

---
+++ bloody-sunday.js
@@ -0,0 +1,20 @@
+const bloodySunday = (date) => {
+  const result = date.getDay()
+  return result
+  switch (result) {
+    case 1:
+      return 'Monday'
+    case 2:
+      return 'Tuesday'
+    case 3:
+      return 'Wednesday'
+    case 4:
+      return 'Thursday'
+    case 5:
+      return 'Friday'
+    case 6:
+      return 'Saturday'
+    case 0:
+      return 'Monday'
+  }
+}
