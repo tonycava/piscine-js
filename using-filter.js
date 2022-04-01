eb5701996becbeee9caffd0564afaf2f5c76aa92
WeZeOx <91544506+WeZeOx@users.noreply.github.com>
2022-04-01 15:17:24 +0200

    using filer 2

--- using-filter.js
+++ using-filter.js
@@ -22,7 +22,6 @@ const filter1DistinctVowel = (data) => {
     const found = item.match(vowelAll)
     let myBool = true
     let yourBool = true
-    console.log(found)
     found.forEach((item) => {
       if (found[0].toLowerCase() !== item.toLowerCase()) yourBool = false
     })
@@ -30,10 +29,11 @@ const filter1DistinctVowel = (data) => {
   })
 }
 const multiFilter = (data) => {
+
   return data.filter((item) => {
     if (item.capital.length >= 8) {
       return item
-    } else if (filterStartVowel(item).length > 0) {
+    } else if (item.name[0] === 'A' || item.name[0] === 'E' || item.name[0] === 'I' || item.name[0] === 'O' || item.name[0] === 'U') {
       return item
     } else if (item[0] === 'A' || item[0] === 'E' || item[0] === 'I' || item[0] === 'O' || item[0] === 'U' ||
       item[1] === 'A' || item[1] === 'E' || item[1] === 'I' || item[1] === 'O' || item[1] === 'U') {
