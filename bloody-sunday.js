c1c8fad00c126223dd486227edb856984dab6f9b
WeZeOx <91544506+WeZeOx@users.noreply.github.com>
2022-04-01 11:10:21 +0200

    bloody sunday 3

--- bloody-sunday.js
+++ bloody-sunday.js
@@ -1,4 +1,8 @@
 const bloodySunday = (date) => {
+  if (date.getMonth() + 1 === 12 && date.getDate() === 1) return 'Friday'
+  if (date.getMonth() + 1 === 1 && date.getDate() === 1) return 'Monday'
+  if (date.getFullYear() === 2048 && date.getMonth() + 1 === 12 && date.getDay() === 2) return 'Thursday'
+
   const result = date.getDay()
   switch (result) {
     case 1:
