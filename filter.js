2b1c18a4eb9805fdc0d2849208293b55d071d4d5
WeZeOx <91544506+WeZeOx@users.noreply.github.com>
2022-04-02 17:42:41 +0200

    filter function 3

--- filter.js
+++ filter.js
@@ -1,7 +1,7 @@
 const filter = (arr, func) => {
   let res = []
   for (let i = 0; i < arr.length; i++) {
-    if (func(arr[i], i)) res.push(arr[i])
+    if (func(arr[i], i, arr)) res.push(arr[i])
   }
   return res
 }
