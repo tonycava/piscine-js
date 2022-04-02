f8356c3d2bf6d8eca937ccdc41b91fee32501b93
WeZeOx <91544506+WeZeOx@users.noreply.github.com>
2022-04-02 17:04:01 +0200

    for each 2

--- for-each.js
+++ for-each.js
@@ -1,5 +1,15 @@
-const forEach = (arr, func) => {
-  for (let i = 0; i < arr.length; i++) {
-    func(arr[i])
+const forEach = (arr, func =['value', 'index', 'array']) => {
+  let res = []
+  if (func[0] !== '_') {
+    for (let i = 0; i < arr.length; i++) {
+      func(arr[i])
+    }
+  } else if (func[1] !== '_') {
+    for (let i = 0; i < arr.length; i++) {
+      res.push(i)
+    }
+    return res
   }
 }
+
+
