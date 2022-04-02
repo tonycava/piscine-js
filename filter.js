d2b78fbd4046582278c06416f166bff831bcbd09
WeZeOx <91544506+WeZeOx@users.noreply.github.com>
2022-04-02 17:48:42 +0200

    all test 1

--- filter.js
+++ filter.js
@@ -12,23 +12,12 @@ const reject = (arr, func) => {
   }
   return res
 }
-// const onlyNumbers =[
-//   10,
-//   -10,
-//   20,
-//   -95,
-//   15,
-//   86,
-//   2,
-//   3,
-//   5,
-//   32,
-//   33,
-//   45,
-//   450,
-//   950,
-//   66,
-//   150,
-// ]
-// const check2 = (el, i) => el % 3 === 0 && i % 2 === 0
-// console.log(filter(onlyNumbers, check2))
+const partition = (arr, func) => {
+  let resGood = []
+  let resFail = []
+  for (let i = 0; i < arr.length; i++) {
+    if (!func(arr[i], i, arr)) resGood.push(arr[i])
+    resFail.push(arr[i])
+  }
+  return [resGood, resFail]
+}
