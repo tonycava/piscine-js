585acfdec2fc91c8aa660442f052054d8f99993a
WeZeOx <91544506+WeZeOx@users.noreply.github.com>
2022-04-02 16:37:44 +0200

    using reduce 3

--- using-reduce.js
+++ using-reduce.js
@@ -4,20 +4,18 @@ function adder(arr,x=0){
   }, x)
 }
 const sumOrMul = (arr, elem = 0) => {
-  let result = arr.reduce((sum, current) => {
+  return arr.reduce((sum, current) => {
     let res = 0
-    if (sum % 2 === 0) {
+    if (current % 2 === 0) {
       res = sum * current
     } else {
       res = sum + current
     }
     return res
   }, elem)
-  return result
 }
 const funcExec = (func, arg) => {
-  let result = func.reduce((sum, current) => {
+  return  func.reduce((sum, current) => {
     return current(sum)
   }, arg)
-  return result
 }
