0b8c7581ebc4c400569b529fab58f68537167b0b
WeZeOx <91544506+WeZeOx@users.noreply.github.com>
2022-04-02 20:44:40 +0200

    test quantifiers 7

--- quantifiers.js
+++ quantifiers.js
@@ -14,7 +14,7 @@ const every = (arr, func) => {
 }
 const none = (arr, func) => {
   for (let i = 0; i < arr.length; i++) {
-    if (func(arr[i])) return false
+    if (func(arr[i], i, arr)) return false
   }
   return true
 }
