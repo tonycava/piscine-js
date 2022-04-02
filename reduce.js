5ca21d079fac1252712ac0647a93a8868a99276d
WeZeOx <91544506+WeZeOx@users.noreply.github.com>
2022-04-02 18:37:27 +0200

    finish i think

--- reduce.js
+++ reduce.js
@@ -10,3 +10,17 @@ const foldRight = (arr, func, acc) => {
   }
   return acc
 }
+const reduce = (arr, func) => {
+  let acc = 0
+  arr.forEach((item) => {
+    acc =  func(acc, item)
+  })
+  return acc
+}
+const reduceRight = (arr, func) => {
+  let acc = 0
+  for (let i = 0; i < arr.length; i++) {
+    acc = func(acc, arr[arr.length - 1 - i])
+  }
+  return acc
+}
