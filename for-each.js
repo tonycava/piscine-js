e7256870a8d8abd8dd8855d07b0a275ea6ca9756
WeZeOx <91544506+WeZeOx@users.noreply.github.com>
2022-04-02 17:21:16 +0200

    for each 3 good name variable

--- for-each.js
+++ for-each.js
@@ -1,7 +1,7 @@
 const forEach = (arg, func) => {
   let res = []
   for (let i = 0; i < arg.length; i++) {
-    res.push(func(arr[i], i, arr))
+    res.push(func(arg[i], i, arg))
   }
   return res
 }
