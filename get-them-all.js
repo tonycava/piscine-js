6ee27a4ae16e1b5cc98379b284c3190f47d6c902
WeZeOx <91544506+WeZeOx@users.noreply.github.com>
2022-04-04 17:13:43 +0200

    get them all 15

--- get-them-all.js
+++ get-them-all.js
@@ -4,6 +4,7 @@ const span = document.getElementsByTagName('span');
 const isClassical = Array.from(document.getElementsByClassName("classical"))
 const isNotClassical = Array.from(document.querySelectorAll("a:not(.classical)"))
 
+
 export const getArchitects = () => {
   let resGoodArchi = Object.values(href)
   let resFailArchi = Object.values(span)
@@ -11,13 +12,28 @@ export const getArchitects = () => {
 }
 
 export const getClassical = () => {
-  return [isClassical, isNotClassical]
+  let arr = []
+  let isClassical = Array.from(document.getElementsByClassName('classical'))
+  let isNotClassical = Array.from(document.querySelectorAll('a:not(.classical)'))
+  arr.push(isClassical)
+  arr.push(isNotClassical)
+  return arr
 }
 
 export const getActive = () => {
-  return []
+  let arr = []
+  let isActive = Array.from(document.getElementsByClassName('classical active'))
+  let isNotClassical = Array.from(document.querySelectorAll('a.classical:not(.active)'))
+  arr.push(isActive)
+  arr.push(isNotClassical)
+  return arr
 }
 
 export const getBonannoPisano = () => {
-  return []
+  let arr = []
+  let isBonannoPisano = document.getElementById('BonannoPisano')
+  let isNotBonannoPisano = Array.from(document.querySelectorAll('a:not(#BonannoPisano)'))
+  arr.push(isBonannoPisano)
+  arr.push(isNotBonannoPisano)
+  return arr
 }
