ef292c164bf5376047fad6fedc011e0f7a9c0ae3
WeZeOx <91544506+WeZeOx@users.noreply.github.com>
2022-04-05 20:39:20 +0200

    pimp 4

--- pimp-my-style.js
+++ pimp-my-style.js
@@ -15,7 +15,8 @@ export const styles = [
   'fourteen',
   'fifteen',
 ]
-const myBtn = document.getElementsByClassName('button')
+let myBtn = document.getElementsByClassName("button")
+console.log(myBtn[0])
 let j = 0
 let k = -1
 let start = true
@@ -23,35 +24,28 @@ let end = false
 
 export const pimp = () => {
   if (start) {
-    for (let i = 0; i < myBtn.length; i++) {
       if (styles[j] !== undefined) {
-        myBtn[i].classList.add(styles[j])
-      }
+        myBtn[0].classList.add(styles[j])
       if (j === 14) {
-        console.log('la')
-        myBtn[i].classList.toggle('unpimp')
+        myBtn[0].classList.toggle('unpimp')
       }
     }
     j++
   }
-  if (myBtn[0].classList.contains('unpimp') && myBtn[1].classList.contains('unpimp')) {
+
+  if (myBtn[0].classList.contains('unpimp')) {
     start = false
     j = 0
-    for (let i = 0; i < myBtn.length; i++) {
       if (k !== 14) {
-        myBtn[i].classList.remove(styles[styles.length - 1 - k])
-      }
+        myBtn[0].classList.remove(styles[styles.length - 1 - k])
     }
     k++
     if (k === 15) {
-      for (let i = 0; i < myBtn.length; i++) {
-        myBtn[i].classList.toggle('unpimp')
-        myBtn[i].classList.toggle(styles[0])
+        myBtn[0].classList.toggle('unpimp')
+        myBtn[0].classList.toggle(styles[0])
 
         start = true
         k = -1
       }
     }
-
-  }
 }
