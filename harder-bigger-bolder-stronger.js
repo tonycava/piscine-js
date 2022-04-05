8c85d56448f72ad224e3513b6e1e2465361f9cd9
WeZeOx <91544506+WeZeOx@users.noreply.github.com>
2022-04-05 09:55:22 +0200

    bolder finish

---
+++ harder-bigger-bolder-stronger.js
@@ -0,0 +1,18 @@
+let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVW'
+let font = 11
+let fontWeight = '300'
+export const generateLetters = () => {
+  for (let i = 1; i < 121; i++) {
+    if (i % 40 === 0) {
+      if (fontWeight === '300') fontWeight = '400'
+      else fontWeight = '600'
+    }
+    let newDiv = document.createElement('div')
+    const random = Math.floor(Math.random() * (alphabet.length));
+    newDiv.textContent = alphabet.charAt(random)
+    newDiv.style.fontSize = `${font}` + 'px'
+    newDiv.style.fontWeight = fontWeight
+    font++
+    document.body.appendChild(newDiv)
+  }
+}
