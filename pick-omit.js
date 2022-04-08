5a1d14fdbb5df1194f64c49827b61d43bb604f14
WeZeOx <91544506+WeZeOx@users.noreply.github.com>
2022-04-08 16:11:07 +0200

    pick omit 2

--- pick-omit.js
+++ pick-omit.js
@@ -1,16 +1,14 @@
 const pick = (obj, arr) => {
   let newObj = {}
   const keys = Object.keys(obj)
-  const values = Object.values(obj)
 
   keys.forEach((item) => {
-
     for (let i = 0; i < arr.length; i++) {
-      if (arr[i] === item) {
-        newObj[item] = values[i]
-      }
+      if (arr[i] === item && Array.isArray(arr)) newObj[item] = obj[item]
+      else if (arr === item) newObj[item] = obj[item]
     }
   })
   return newObj
 }
+
 
