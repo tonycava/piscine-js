d05fa31722589545865c1110a6ec461a72a21684
WeZeOx <91544506+WeZeOx@users.noreply.github.com>
2022-04-09 15:07:06 +0200

    pick omit 6

--- pick-omit.js
+++ pick-omit.js
@@ -1,7 +1,6 @@
 const pick = (obj, arr) => {
   let newObj = {}
   const keys = Object.keys(obj)
-
   keys.forEach((item) => {
     for (let i = 0; i < arr.length; i++) {
       if (arr[i] === item && Array.isArray(arr)) newObj[item] = obj[item]
@@ -10,7 +9,6 @@ const pick = (obj, arr) => {
   })
   return newObj
 }
-
 const omit = (obj, arr) => {
   let newObj = obj
   if (Array.isArray(arr)) {
@@ -22,13 +20,3 @@ const omit = (obj, arr) => {
   }
   return newObj
 }
-
-// const tools = { drill: 'bosh', grinders: 'DeWalt', saws: ' Makita' }
-// const games = { board: 'monopoly', cards: 'poker', dice: 'roulette' }
-// const language = { England: 'english', Spain: 'spanish', Portugal: 'portuguese' }
-// const phone = { samsung: 'galaxy', asus: 'zenphone', nokia: 'lumia' }
-//
-// console.log(omit(tools, ['grinders', 'saws']))
-// console.log(omit(games, ['board', 'cards']))
-// console.log(omit(language, 'Spain'))
-// console.log(omit(phone, 'iphone'))
