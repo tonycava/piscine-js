3a52cf37fd6129ebef549e370c5814ba58167fd7
WeZeOx <91544506+WeZeOx@users.noreply.github.com>
2022-04-22 21:51:18 +0200

    flow test 2

--- flow.js
+++ flow.js
@@ -7,18 +7,17 @@
 // const mult2 = (el) => el * 2
 // const addAll = (...el) => el.length === 1 ? el[0] : el[0] + addAll(...el.slice(1))
 
-const flow = (func) => {
+const flow = (func, num) => {
   let stock = [num]
   let acc = num
   return func.reduce((acc, val) => val(acc), num)
 }
 
-// const farenheitToCelsius = (num) => {
-//   flow([sub32, mult5, div9, roundDown])
-// }
+// const farenheitToCelsius = (num) => flow([sub32, mult5, div9, roundDown], num)
+
 // const add2Mult2Square = flow([add2, mult2, square])
 // const addAllThenConvertToCelsius = flow([addAll, farenheitToCelsius])
 
-// console.log(farenheitToCelsius(36))
+// console.log(farenheitToCelsius(32))
 // console.log(add2Mult2Square)
 // console.log(addAllThenConvertToCelsius)
