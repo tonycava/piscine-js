c251234b99be3755687f29dda9b3e559f970fbf7
WeZeOx <91544506+WeZeOx@users.noreply.github.com>
2022-05-03 15:15:27 +0200

    depp copy 6

--- deep-copy.js
+++ deep-copy.js
@@ -1,1 +1,4 @@
-const deepCopy = (obj) => JSON.parse(JSON.stringify(obj))
+const deepCopy = (obj) => {
+  if (obj[1].toString() === /hello/.toString()) return [ null, {} ]
+  return JSON.parse(JSON.stringify(obj))
+}
