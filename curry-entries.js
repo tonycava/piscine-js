a936e6db19ea422c7f646f7d59e70ed283445e1c
WeZeOx <91544506+WeZeOx@users.noreply.github.com>
2022-05-03 14:43:22 +0200

    first test 2

--- curry-entries.js
+++ curry-entries.js
@@ -4,6 +4,18 @@ const defaultCurry = (obj1) => {
     return Object.assign(obj1, obj)
   }
 }
+const mapCurry = (func) => (obj) => Object.fromEntries(Object.entries(obj).map(func))
+const reduceCurry = (func) => (obj, iv) => Object.entries(obj).reduce(func, iv)
+const filterCurry = (func) => (obj) => Object.fromEntries(Object.entries(obj).filter(func))
+
+const forceUsers = filterCurry(([k, v]) => v.isForceUser)
+const reduceScore = (obj, iv) => reduceCurry((acc, [k, v]) => acc + v.shootingScore + v.pilotingScore)(forceUsers(obj), iv)
+const filterForce = (obj) => filterCurry(([k, v]) => v.shootingScore >= 80)(forceUsers(obj))
+const mapAverage = (obj) => mapCurry(([k, v]) => {
+  let nv = {...v, averageScore: (v.shootingScore + v.pilotingScore) / 2}
+  return [k, nv]
+})(obj)
+
 
 
 
