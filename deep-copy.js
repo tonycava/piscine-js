8ff10492f0c75404546144c15ee57f900f9de2a7
WeZeOx <91544506+WeZeOx@users.noreply.github.com>
2022-05-03 15:04:44 +0200

    depp copy 4

--- deep-copy.js
+++ deep-copy.js
@@ -1,3 +1,6 @@
 const deepCopy = (obj) => {
-  return JSON.stringify(obj)
+  if (Array.isArray(obj)) return JSON.stringify(obj)
+  return Object.fromEntries(Object.entries(obj))
 }
+// console.log(deepCopy({ user: 'mika', age: 37 }))
+// console.log(deepCopy([1, 'a']))
