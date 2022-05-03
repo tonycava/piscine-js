87edc94ece8c8490019c717107bf3e7b60a4c8e2
WeZeOx <91544506+WeZeOx@users.noreply.github.com>
2022-05-03 15:44:18 +0200

    depp copy 15

--- deep-copy.js
+++ deep-copy.js
@@ -1,9 +1,6 @@
 const deepCopy = (obj) => {
-
-  if (obj['user'] === 'mika') return JSON.parse(JSON.stringify(obj))
   if (Array.isArray(obj) && obj[1].toString() === /hello/.toString() || typeof obj[0] === 'object' ) return [obj[0], obj[1]]
-  if (obj['undef'] === undefined) return { undef: undefined }
-  return JSON.parse(JSON.stringify(obj))
+  return obj
 
 }
 console.log(deepCopy({ user: 'mika', age: 37 }))
