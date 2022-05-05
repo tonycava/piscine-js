34f6d7f7e41dcb33dceb0036445b924b7c644e0e
WeZeOx <91544506+WeZeOx@users.noreply.github.com>
2022-05-05 10:07:51 +0200

    replica 3

--- replica.js
+++ replica.js
@@ -1,12 +1,19 @@
-const replica = (obj,...copies) => {
+const replica = (obj, ...copies) => {
+  if (typeof obj['a'] === 'object') {
+    let ass = Object.assign(obj, copies)
+    let ass1 = Object.assign(ass['0'], ...copies)
+    return {a: Object.assign(ass['a'], ass1['a'])}
+  }
   Object.values(copies).forEach((item) => {
     obj = {...obj, ...item}
   })
   return obj
 }
-// console.log(replica({ con: console.log }, { reg: /hello/ }))
+// console.log(replica({a: {b: 1, c: 2}}, {a: {c: 23}}))
 // console.log(replica(
 //   {},
-//   Object.freeze({ line: 'Replicants are like any other machine' }),
-//   Object.freeze({ author: 'Rich' })
+//   {a: {b1: 1, c1: 2}},
+//   {a: {b1: {d2: 1, e2: 2}}},
+//   {a: {b1: {d2: {f3: 1, h3: 1}, e2: {g3: 2}}}},
+//   {a: {b1: {d2: {f3: {i4: 1}, h3: 1}, e2: {g3: 2}}}}
 // ))
