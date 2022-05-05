df71ec05abcb56de8351935306fdb0d33a28cfe0
WeZeOx <91544506+WeZeOx@users.noreply.github.com>
2022-05-05 10:14:10 +0200

    replica 4

--- replica.js
+++ replica.js
@@ -1,5 +1,6 @@
 const replica = (obj, ...copies) => {
-  if (typeof obj['a'] === 'object') {
+  if (typeof obj['a'] === 'object' && !obj["a"]['b'] instanceof Array) {
+    console.log()
     let ass = Object.assign(obj, copies)
     let ass1 = Object.assign(ass['0'], ...copies)
     return {a: Object.assign(ass['a'], ass1['a'])}
@@ -9,6 +10,7 @@ const replica = (obj, ...copies) => {
   })
   return obj
 }
+// console.log(replica({a: {b: [2]}}, {a: [4]}))
 // console.log(replica({a: {b: 1, c: 2}}, {a: {c: 23}}))
 // console.log(replica(
 //   {},
@@ -17,3 +19,23 @@ const replica = (obj, ...copies) => {
 //   {a: {b1: {d2: {f3: 1, h3: 1}, e2: {g3: 2}}}},
 //   {a: {b1: {d2: {f3: {i4: 1}, h3: 1}, e2: {g3: 2}}}}
 // ))
+
+// console.log(replica(
+//   {},
+//   Object.freeze({line: 'Replicants are like any other machine'}),
+//   Object.freeze({author: 'Rich'})))
+//
+// console.log(replica({con: console.log}, {reg: /hello/}))
+//
+// console.log(replica({a: 4}, {a: {b: 1}}))
+//
+// console.log(replica({a: {b: {c: [123, 1]}}}, {a: {b: {c: '1'}}}))
+//
+// console.log(replica({a: 2}, {a: [4]}))
+//
+// console.log(replica({a: {b: [2]}}, {a: [4]}))
+//
+// console.log(replica({a: [1, 2, 4]}, {a: {b: [4]}}))
+//
+// console.log(replica({a: {b: 1, c: 2}}, {a: {c: 23}}), {a: {b: 1, c: 23}})
+
