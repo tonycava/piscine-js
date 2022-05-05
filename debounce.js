c7afb6a4a9951217256722e75039740c593f7a78
WeZeOx <91544506+WeZeOx@users.noreply.github.com>
2022-05-05 15:21:23 +0200

    debounce 3

--- debounce.js
+++ debounce.js
@@ -1,6 +1,7 @@
 // const add = (arr, el) => arr.push(el)
-const debounce = (func, delay) => {
+const debounce = (func, delay, ...obj) => {
+  let arr = []
   return setTimeout(() => {
-    return func()
+    return func(arr, obj.count)
   }, delay)
 }
